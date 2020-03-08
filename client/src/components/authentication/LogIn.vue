761e181bd201e58872a8e2d8ad423d5c31fd499f
kouzana fedi <fedi.kouzana1@gmail.com>
2020-03-08 01:39:34 +0100

    Created basic log in template

--- client/src/components/authentication/LogIn.vue
+++ client/src/components/authentication/LogIn.vue
@@ -1,16 +1,37 @@
 <template>
-  <div>
-      LogIn :)
+  <div id="LogIn-main-container">
+      <label for="email">Email address</label>
+      <input v-model="email" type="email" name="email" id="email">
+      <label for="password">Password</label>
+      <input v-model="password" type="password" name="password" id="password">
+      <button type="submit" @click="logIn">Log In</button>
+      <div>Not a member?
+        <a>Sign Up</a>
+      </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    name:'LogIn'
-})
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: 'LogIn' as string,
+    data(){
+      return{
+        email : '' as string,
+        password : '' as string
+      }
+    } ,
+    methods:{
+      logIn(): void{
+        return null;
+      }
+    }
+  })
 </script>
 
-<style>
-
+<style lang="scss">
+  #LogIn-main-container{
+    display: flex;
+    flex-direction: column;
+  }
 </style>
