932d2f8be78c01d9e9f3a5f34c820da443091171
kouzana fedi <fedi.kouzana1@gmail.com>
2020-03-08 02:07:05 +0100

    Created basic sign up template

--- client/src/components/authentication/SignUp.vue
+++ client/src/components/authentication/SignUp.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>
-      SignUp :)
+  <div id="SignUp-main-container">
+      <label for="Name">Name</label>
+      <input v-model="name" type="text" name="name" id="name">
+      <label for="email">Email address</label>
+      <input v-model="email" type="email" name="email" id="email">
+      <label for="password">Password</label>
+      <input v-model="password" type="password" name="password" id="password">
+      <button type="submit" @click="signUp">Log In</button>
+      <div>Already a member?
+        <a>Log In</a>
+      </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    name:'SignUp'
-})
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: 'SignUp' as string,
+    data(){
+      return{
+        name:'' as string,
+        email : '' as string,
+        password : '' as string
+      }
+    } ,
+    methods:{
+      signUp(): void{
+        return null;
+      }
+    }
+  })
 </script>
 
-<style>
-
+<style lang="scss">
+  #SignUp-main-container{
+    display: flex;
+    flex-direction: column;
+  }
 </style>
